f00431e16daf3944759c69f110d32d1be2edbcf1
Corey Osman <corey@nwops.io>
2022-05-03 19:39:46 -0500

    Add test case to cover when authselect_profile_features is present

--- spec/classes/authselect_spec.rb
+++ spec/classes/authselect_spec.rb
@@ -97,6 +97,30 @@ describe 'authselect' do
         # it { pp catalogue.resources }
         it { is_expected.to contain_exec('authselect set profile=testing features=[a, b]') }
       end
+
+      context 'yes package, yes profile, with options, already present' do
+        before(:each) do
+          os_facts[:authselect_profile_features] = ['a', 'b' ]
+        end
+
+        let(:params) do
+          {
+            'package_manage' => true,
+            'profile_manage' => true,
+            'profile' => 'testing',
+            'profile_options' => ['b', 'a'],
+          }
+        end
+
+        it { is_expected.to compile }
+        it { is_expected.to have_package_resource_count(1) }
+        it { is_expected.to have_exec_resource_count(1) }
+        it { is_expected.to contain_package('authselect').with_ensure('present') }
+        # it { pp catalogue.resources }
+        it { is_expected.to contain_exec('authselect set profile=testing features=[a, b]').with({
+          unless: 'authselect check'
+        }) }
+      end
     end
   end
 end
