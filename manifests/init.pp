3b1115443afc0ff266cb3ae605f19c990508b498
Mike Riddle <michael.riddle@onyxpoint.com>
2023-04-27 15:43:05 -0400

    Some minor updates for consistency

--- manifests/init.pp
+++ manifests/init.pp
@@ -57,13 +57,13 @@ class authselect (
     }
   }
 
+  if $profile_manage and $package_ensure != 'absent' {
+    include 'authselect::config'
+  }
+
   $custom_profiles.each |$key, $value| {
     authselect::custom_profile { $key:
       * => $value,
     }
-  }
-
-  if $profile_manage and $package_ensure != 'absent' {
-    include 'authselect::config'
   }
 }
