fc5cda9500057f8d7bd4a40fbdad788ac0fa0bfd
Corporate Gadfly <haroon.rafique@gmail.com>
2026-09-13 18:34:32 -0400

    prefer confine over if-statement
    Signed-off-by: Corporate Gadfly <haroon.rafique@gmail.com>

--- lib/facter/authselect_profile_features.rb
+++ lib/facter/authselect_profile_features.rb
@@ -5,18 +5,14 @@ require 'yaml'
 Facter.add(:authselect_profile_features) do
   # https://docs.openvoxproject.org/openfact/latest/
   confine kernel: 'Linux'
-  retval = nil
+  confine { Facter::Core::Execution.which('authselect') }
 
   setcode do
-    if Facter::Core::Execution.which('authselect')
-      begin
-        cmd_out = Facter::Core::Execution.execute('authselect current')
-        retval = YAML.safe_load(cmd_out)['Enabled features']
-        retval = [] if retval == 'None'
-      rescue StandardError
-        nil
-      end
-    end
+    cmd_out = Facter::Core::Execution.execute('authselect current')
+    retval = YAML.safe_load(cmd_out)['Enabled features']
+    retval = [] if retval == 'None'
     retval
+  rescue StandardError
+    nil
   end
 end
